0f2780a6bcdaf93c71fdc943feb5c58c2dd53d98
MinDev <tranquandev@gmail.com>
2025-03-03 19:18:12 +0700

    crud user table

--- src/shared/ui/organisms/PaginationTable.tsx
+++ src/shared/ui/organisms/PaginationTable.tsx
@@ -13,7 +13,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "~/shared/ui/molecules/select";
+} from "~/shared/ui/molecules/Select";
 
 interface DataTablePaginationProps<TData> {
   table: Table<TData>;
@@ -23,7 +23,7 @@ export function PaginationTable<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
+    <div className="flex items-center justify-between px-2 mt-4">
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
